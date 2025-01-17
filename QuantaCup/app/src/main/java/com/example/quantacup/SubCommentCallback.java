ebcbad91aa4db4a550e662c4af84e05246f39d02
ohhffr <ffrfangfarong@163.com>
2025-01-17 18:54:08 +0800

    删除了QuantaCap文件夹

--- QuantaCup/app/src/main/java/com/example/quantacup/SubCommentCallback.java
+++
@@ -1,5 +0,0 @@
-package com.example.quantacup;
-
-public interface SubCommentCallback {
-    void onUserInfoFetched(String username, String userAvatarURL); // 更新方法签名
-}
--- QuantaCup/app/src/main/java/com/example/quantacup/adapter/OnSubCommentClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.quantacup.adapter;
-
-import com.example.quantacup.bean.SubComment;
-
-// SubCommentAdapter.java
-// 定义一个接口用于处理子评论点击事件
-public interface OnSubCommentClickListener {
-    void onSubCommentClick(SubComment subComment);
-}
